17880b213db6f1a27ebc027c5be2afc522c04c4e
Joe0047 <joe210150@gmail.com>
2018-01-06 16:14:27 +0800

    I update the movie_intro

---
+++ joe/app/Data/DataFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Data;
+
+class DataFactory {
+	private $db;
+
+	public function __construct() {
+		$this->connect();
+	}
+
+	public function connect() {
+		try {
+			$config = parse_ini_file(BASE_PATH . DIRECTORY_SEPARATOR . 'config.ini');
+			//$dsn = $config['DB_CONNECTION'] . ':host=' . $config['DB_HOST'] . ';port=' . $config['DB_PORT'] . ';daname=' . $config['DB_DATABASE'];
+			$dsn = sprintf("%s:host=%s;port=%s;dbname=%s", $config['DB_CONNECTION'], $config['DB_HOST'], $config['DB_PORT'], $config['DB_DATABASE']);
+			$this->db = new \PDO($dsn, $config['DB_USERNAME'], $config['DB_PASSWORD']);
+			$this->db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+			//echo 'success';
+		}
+		catch (\PDOException $e) {
+			print "Couldn't connect to the database: " . $e->getMessage();
+		}
+	}
+
+	public function disconnect() {
+		$this->db = '';
+	}
+
+	public function getDB() {
+		return $this->db;
+	}
+
+	public function __destruct() {
+
+	}
+
+}
